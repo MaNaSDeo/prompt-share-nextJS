3bada7951a6e444719e73df9bf419ca95975c887
manasdeo <rmnslogin@gmail.com>
2024-08-13 23:47:52 +0530

    Landing text

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Disover and share <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for modern wold to
+        discover, create and share creative prompts
+      </p>
+    </section>
+  );
 }
 
 export default Home;
